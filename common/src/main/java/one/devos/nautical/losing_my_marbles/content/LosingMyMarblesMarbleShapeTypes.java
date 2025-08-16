f97819c9b3016b1682973e5aae8f1805d4e672c9
TropheusJ <jverrellijr@icloud.com>
2025-08-16 00:36:48 -0400

    if not cube, why cube shaped

--- common/src/main/java/one/devos/nautical/losing_my_marbles/content/LosingMyMarblesMarbleShapeTypes.java
+++ common/src/main/java/one/devos/nautical/losing_my_marbles/content/LosingMyMarblesMarbleShapeTypes.java
@@ -3,12 +3,14 @@ package one.devos.nautical.losing_my_marbles.content;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import one.devos.nautical.losing_my_marbles.LosingMyMarbles;
+import one.devos.nautical.losing_my_marbles.content.marble.data.shape.CubeMarbleShape;
 import one.devos.nautical.losing_my_marbles.content.marble.data.shape.MarbleShape;
 import one.devos.nautical.losing_my_marbles.content.marble.data.shape.SphereMarbleShape;
 
 public final class LosingMyMarblesMarbleShapeTypes {
 	public static void init() {
 	    register("sphere", SphereMarbleShape.TYPE);
+		register("cube", CubeMarbleShape.TYPE);
 	}
 
 	private static void register(String name, MarbleShape.Type<?> type) {
---
+++ common/src/main/java/one/devos/nautical/losing_my_marbles/content/marble/data/shape/CubeMarbleShape.java
@@ -0,0 +1,32 @@
+package one.devos.nautical.losing_my_marbles.content.marble.data.shape;
+
+import com.github.stephengold.joltjni.BoxShape;
+import com.github.stephengold.joltjni.Vec3;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import one.devos.nautical.losing_my_marbles.framework.network.LosingMyMarblesStreamCodecs;
+
+public record CubeMarbleShape(float sideLength) implements MarbleShape {
+	public static final MapCodec<CubeMarbleShape> CODEC = Codec.floatRange(1 / 16f, 2)
+			.fieldOf("side_length")
+			.xmap(CubeMarbleShape::new, CubeMarbleShape::sideLength);
+
+	public static final StreamCodec<ByteBuf, CubeMarbleShape> STREAM_CODEC = LosingMyMarblesStreamCodecs.floatRange(1 / 8f, 4)
+			.map(CubeMarbleShape::new, CubeMarbleShape::sideLength);
+
+	public static final Type<CubeMarbleShape> TYPE = new Type<>(CODEC, STREAM_CODEC);
+
+	@Override
+	public CreatedShape createJoltShape(float scale) {
+		float halfExtent = (this.sideLength / 2) * scale;
+		return new CreatedShape(new BoxShape(halfExtent), new Vec3(0, halfExtent, 0));
+	}
+
+	@Override
+	public Type<?> type() {
+		return TYPE;
+	}
+}
