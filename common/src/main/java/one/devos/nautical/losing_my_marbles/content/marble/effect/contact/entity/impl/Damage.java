d3a8354b9ba70b2dc2cb4f2f73939fdf1a5ffd93
TropheusJ <jverrellijr@icloud.com>
2025-08-18 06:00:17 -0400

    add velocity scaling to damage contact effect

--- common/src/main/java/one/devos/nautical/losing_my_marbles/content/marble/effect/contact/entity/impl/Damage.java
+++ common/src/main/java/one/devos/nautical/losing_my_marbles/content/marble/effect/contact/entity/impl/Damage.java
@@ -1,5 +1,8 @@
 package one.devos.nautical.losing_my_marbles.content.marble.effect.contact.entity.impl;
 
+import java.util.Optional;
+
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
@@ -11,11 +14,14 @@ import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 import one.devos.nautical.losing_my_marbles.content.marble.MarbleEntity;
 import one.devos.nautical.losing_my_marbles.content.marble.effect.contact.entity.EntityContactEffect;
+import one.devos.nautical.losing_my_marbles.framework.data.LosingMyMarblesCodecs;
 
-public record Damage(Holder<DamageType> type, FloatProvider amount) implements EntityContactEffect {
+public record Damage(Holder<DamageType> type, FloatProvider amount, boolean velocityScaled, Optional<Float> minimum) implements EntityContactEffect {
 	public static final MapCodec<Damage> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
 			DamageType.CODEC.fieldOf("damage_type").forGetter(Damage::type),
-			FloatProvider.codec(0, Float.MAX_VALUE).fieldOf("amount").forGetter(Damage::amount)
+			FloatProvider.codec(0, Float.MAX_VALUE).fieldOf("amount").forGetter(Damage::amount),
+			Codec.BOOL.optionalFieldOf("velocity_scaled", false).forGetter(Damage::velocityScaled),
+			LosingMyMarblesCodecs.POSITIVE_FLOAT.optionalFieldOf("minimum").forGetter(Damage::minimum)
 	).apply(i, Damage::new));
 
 	@Override
@@ -25,6 +31,13 @@ public record Damage(Holder<DamageType> type, FloatProvider amount) implements E
 
 		DamageSource source = new DamageSource(this.type, entity, entity.getOwner());
 		float amount = this.amount.sample(entity.getRandom());
+
+		if (this.velocityScaled) {
+			amount *= (float) entity.getDeltaMovement().length();
+		}
+
+		if (this.minimum.isPresent() && amount < this.minimum.get())
+			return;
 
 		target.hurtServer(level, source, amount);
 	}
