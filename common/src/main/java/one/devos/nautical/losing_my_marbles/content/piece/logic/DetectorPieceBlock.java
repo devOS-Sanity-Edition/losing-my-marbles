5675d9f677814153dc24762a6cdff6b94c37bd39
TropheusJ <jverrellijr@icloud.com>
2025-08-22 04:08:11 -0400

    make detector stay active instead of pulsing

--- common/src/main/java/one/devos/nautical/losing_my_marbles/content/piece/logic/DetectorPieceBlock.java
+++ common/src/main/java/one/devos/nautical/losing_my_marbles/content/piece/logic/DetectorPieceBlock.java
@@ -4,19 +4,24 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.phys.AABB;
 import one.devos.nautical.losing_my_marbles.content.marble.MarbleEntity;
 import one.devos.nautical.losing_my_marbles.content.piece.StraightPieceBlock;
-import one.devos.nautical.losing_my_marbles.framework.block.MarbleListeningBlock;
 
-public final class DetectorPieceBlock extends StraightPieceBlock implements MarbleListeningBlock {
+public final class DetectorPieceBlock extends StraightPieceBlock {
 	public static final int ACTIVE_TICKS = 10;
 	public static final BooleanProperty ACTIVE = BooleanProperty.create("active");
+
+	public static final AABB DETECTION_AREA = box(4, 8, 6, 12, 14, 10).toAabbs().getFirst();
 
 	public DetectorPieceBlock(Properties properties) {
 		super(properties);
@@ -46,14 +51,21 @@ public final class DetectorPieceBlock extends StraightPieceBlock implements Marb
 	}
 
 	@Override
-	public void marbleEntered(ServerLevel level, BlockState state, BlockPos pos, MarbleEntity entity) {
+	protected void entityInside(BlockState state, Level level, BlockPos pos, Entity entity, InsideBlockEffectApplier applier) {
+		if (!(level instanceof ServerLevel serverLevel) || !(entity instanceof MarbleEntity))
+			return;
+
+		AABB detectionArea = DETECTION_AREA.move(pos);
+		if (!entity.getBoundingBox().intersects(detectionArea))
+			return;
+
 		if (!state.getValue(ACTIVE)) {
-			toggle(level, state, pos);
+			toggle(serverLevel, state, pos);
 		}
 
 		// cancel any previous scheduled ticks
-		level.getBlockTicks().clearArea(new BoundingBox(pos));
-		level.scheduleTick(pos, this, ACTIVE_TICKS);
+		serverLevel.getBlockTicks().clearArea(new BoundingBox(pos));
+		serverLevel.scheduleTick(pos, this, ACTIVE_TICKS);
 	}
 
 	private static void toggle(ServerLevel level, BlockState state, BlockPos pos) {
